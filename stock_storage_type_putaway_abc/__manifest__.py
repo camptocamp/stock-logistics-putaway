1b38024963da29be2180286158e2ccba3104968f
Sébastien Alix <sebastien.alix@camptocamp.com>
2025-05-06 12:02:28 +0200

    [IMP] stock_storage_type_putaway_abc: pre-commit execution

--- stock_storage_type_putaway_abc/__manifest__.py
+++ stock_storage_type_putaway_abc/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": "Advanced storage strategy ABC for WMS",
     "version": "16.0.1.0.0",
     "category": "Warehouse Management",
-    "website": "https://github.com/OCA/wms",
+    "website": "https://github.com/OCA/stock-logistics-putaway",
     "author": "Camptocamp, BCIM, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
