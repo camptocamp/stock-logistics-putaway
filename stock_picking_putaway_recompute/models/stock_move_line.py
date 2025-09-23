cbce47c65a4f7506b93a30764867928ef677ecdd
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2025-09-23 14:10:19 +0200

    stock_picking_putaway_recompute: only filter lines on action
    Only enforce the filtering of the lines when calling the action.
    When calling the private method only check the related picking
    type has the flag for recompute.

--- stock_picking_putaway_recompute/models/stock_move_line.py
+++ stock_picking_putaway_recompute/models/stock_move_line.py
@@ -49,16 +49,17 @@ class StockMoveLine(models.Model):
         Launches the computation of putaways on operations that are
         allowed to.
         """
-        to_recompute_lines = self._filtered_for_putaway_recompute()
+        to_recompute_lines = self.filtered(
+            lambda line: line.picking_id._can_recompute_putaway()
+        )
         # Reset location destinations to their move destination
         # First, protect the field from recomputations as
         # value will be reaffected afterwards.
-        with to_recompute_lines.env.protecting(
-            ["location_dest_id"], to_recompute_lines
-        ):
+        with self.env.protecting(["location_dest_id"], to_recompute_lines):
             for line in to_recompute_lines:
                 line.location_dest_id = line.move_id.location_dest_id
         to_recompute_lines._apply_putaway_strategy()
 
     def action_recompute_putaways(self):
-        self._recompute_putaways()
+        to_recompute_lines = self._filtered_for_putaway_recompute()
+        to_recompute_lines._recompute_putaways()
